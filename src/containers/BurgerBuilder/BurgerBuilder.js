ee1c8874f025a5170cc58095159dcd300c2bb931
Carlos Canizal <carloscanizal@Carloss-MacBook-Pro-3.local>
2019-04-06 00:21:16 -0600

    Adds continue and cancel button to orden summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,6 +37,10 @@ class BurgerBuilder extends Component{
         this.setState({ purchasing: false});
     }
 
+    purchaseContinueHandler = ()=>{
+        alert('You Continue!');
+    }
+
     updatePurchaseState = (ingredients)=>{
 
         const sum = Object.keys(ingredients)
@@ -96,7 +100,10 @@ class BurgerBuilder extends Component{
         return(
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary  ingredients={this.state.ingredients}/>
+                    <OrderSummary  
+                        purchaseCancel={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
