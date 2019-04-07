3be89ca10fe186ee2b57d6479cb4bad43c50b7d3
Carlos Canizal <carloscanizal@Carloss-MacBook-Pro-3.local>
2019-04-06 19:37:00 -0600

    Adds toogle hamburger menu button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,17 +7,24 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component{
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerCloseHandler = () =>{
         this.setState({ showSideDrawer: false });
     }
 
+    sideDrawerOpenHandler = () =>{
+        this.setState(( prevState )=>{
+                return { showSideDrawer: !prevState.showSideDrawer }
+            }
+        );
+    }
+
     render(){
         return(
             <Aux>
-                <Toolbar />
+                <Toolbar sideDrawerClicked={this.sideDrawerOpenHandler} />
                 <SideDrawer 
                     open={this.state.showSideDrawer} 
                     closed={this.sideDrawerCloseHandler} 
